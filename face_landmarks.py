5053b844f31479ee1e5f210d6bc3aaf2df06078a
Sumit Tyagi <sumittyagi1998@gmail.com>
2019-07-10 01:29:55 +0530

    face-landmarks added

---
+++ face_landmarks.py
@@ -0,0 +1,69 @@
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import dlib
+import cv2
+
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+detector = dlib.get_frontal_face_detector()
+#predictor = dlib.shape_predictor(args["shape_predictor"])
+predictor = dlib.shape_predictor("models/full_face_68.dat")
+
+
+# https://github.com/Luca96/dlib-minified-models/tree/master/face_landmarks
+
+
+cap = cv2.VideoCapture(1)
+ 
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+ 
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  image = imutils.resize(frame, width=500)
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  rects = detector(gray, 1)
+  if ret == True:
+    for (i, rect) in enumerate(rects):
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+    
+        # convert dlib's rectangle to a OpenCV-style bounding box
+        # [i.e., (x, y, w, h)], then draw the face bounding box
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+        # show the face number
+        cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    
+        # loop over the (x, y)-coordinates for the facial landmarks
+        # and draw them on the image
+        for (x, y) in shape:
+            cv2.circle(image, (x, y), 1, (0, 0, 255), thickness=1)
+
+        # Display the resulting frame
+    cv2.imshow('Frame',image)
+ 
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+ 
+  # Break the loop
+  else: 
+    break
+ 
+# When everything done, release the video capture object
+cap.release()
+ 
+# Closes all the frames
+cv2.destroyAllWindows()
